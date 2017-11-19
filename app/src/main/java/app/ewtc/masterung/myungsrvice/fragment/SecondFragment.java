7912ce15dd5fc8ede09cb00e4b0615566150fdb7
MasterUNG <phrombutr@gmail.com>
2017-11-19 14:01:44 +0700

    Show Date Success

--- app/src/main/java/app/ewtc/masterung/myungsrvice/fragment/SecondFragment.java
+++ app/src/main/java/app/ewtc/masterung/myungsrvice/fragment/SecondFragment.java
@@ -3,11 +3,18 @@ package app.ewtc.masterung.myungsrvice.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import app.ewtc.masterung.myungsrvice.R;
+import app.ewtc.masterung.myungsrvice.utility.GetJSON;
+import app.ewtc.masterung.myungsrvice.utility.MyConstant;
 
 /**
  * Created by masterung on 18/11/2017 AD.
@@ -15,6 +22,69 @@ import app.ewtc.masterung.myungsrvice.R;
 
 public class SecondFragment extends Fragment{
 
+//    Explicit
+    private String jsonString, dateString,
+        rateString, usdString, answerString;
+    private String jsonRateString;
+    private double rateADouble;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Read All JSON to Class
+        readJSON();
+
+//        Show Date
+        showDate();
+
+
+    }   // Main Method
+
+    private void showDate() {
+        TextView textView = getView().findViewById(R.id.txtShowDate);
+
+        try {
+
+            JSONArray jsonArray = new JSONArray("[" + jsonString + "]");
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+            dateString = jsonObject.getString("date");
+            Log.d("19novV1", "Date ==> " + dateString);
+
+            textView.setText(getText(R.string.date) + dateString);
+
+            jsonRateString = "[" + jsonObject.getString("rates") + "]";
+            Log.d("19novV1", "jsonRate ==> " + jsonRateString);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void readJSON() {
+
+        String tag = "19novV1";
+        MyConstant myConstant = new MyConstant();
+
+        try {
+
+            GetJSON getJSON = new GetJSON(getActivity());
+            getJSON.execute(myConstant.getUrlJSON());
+
+            jsonString = getJSON.get();
+            Log.d(tag, "JSON ==> " + jsonString);
+
+        } catch (Exception e) {
+            Log.d(tag, "e ==> " + e.toString());
+        }
+
+    }
+
+    //    Create View คือการสร้าง หน้ากาก
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,4 +92,5 @@ public class SecondFragment extends Fragment{
                 container, false);
         return view;
     }
+
 }   // Main Class
--- app/src/main/java/app/ewtc/masterung/myungsrvice/utility/GetJSON.java
+++ app/src/main/java/app/ewtc/masterung/myungsrvice/utility/GetJSON.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 /**
  * Created by masterung on 19/11/2017 AD.
@@ -26,13 +27,16 @@ public class GetJSON extends AsyncTask<String, Void, String>{
             OkHttpClient okHttpClient = new OkHttpClient();
             Request.Builder builder = new Request.Builder();
             Request request = builder.url(strings[0]).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
 
 
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
 
 
-        return null;
+
     }
 }   // Main Class
